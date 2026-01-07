bd26a3c881427f77616d6b640c514759f45f4ce7
Kotimish <kotov.mikhail.96@gmail.com>
2026-01-07 19:33:12 +0300

    change migration for task in project schema (assignee int to foreign key participants)

--- apps/monolith/alembic/versions/2026_01_07_0011-6aa6f418170e_create_project_tasks_table.py
+++ apps/monolith/alembic/versions/2026_01_07_0011-6aa6f418170e_create_project_tasks_table.py
@@ -1,8 +1,8 @@
 """create project tasks table
 
-Revision ID: 1c189ea4bcd5
+Revision ID: 6aa6f418170e
 Revises: 55fea1ab5b36
-Create Date: 2025-12-14 23:21:10.958827
+Create Date: 2026-01-07 00:11:43.889830
 
 """
 from typing import Sequence, Union
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '1c189ea4bcd5'
+revision: str = '6aa6f418170e'
 down_revision: Union[str, Sequence[str], None] = '55fea1ab5b36'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -35,62 +35,65 @@ def upgrade() -> None:
             'description',
             sa.Text(),
             server_default='',
-            nullable=True,
+            nullable=True
         ),
         sa.Column(
             'assignee_id',
             sa.Integer(),
-            nullable=True,
+            nullable=True
         ),
         sa.Column(
             'project_id',
             sa.Integer(),
-            nullable=False,
+            nullable=False
         ),
         sa.Column(
             'status_id',
             sa.Integer(),
-            nullable=False,
+            nullable=False
         ),
         sa.Column(
             'sprint_id',
             sa.Integer(),
-            nullable=True,
+            nullable=True
         ),
         sa.Column(
             'created_at',
             sa.DateTime(),
             server_default=sa.text('now()'),
-            nullable=False,
+            nullable=False
         ),
         sa.Column(
             'updated_at',
             sa.DateTime(),
             server_default=sa.text('now()'),
-            nullable=False,
+            nullable=False
+        ),
+        sa.ForeignKeyConstraint(
+            ['assignee_id'],
+            ['project.participants.id'],
+            name=op.f('fk_tasks_assignee_id_participants')
         ),
         sa.ForeignKeyConstraint(
             ['project_id'],
             ['project.projects.id'],
-            name=op.f('fk_tasks_project_id_projects'
-                      ),
+            name=op.f('fk_tasks_project_id_projects')
         ),
         sa.ForeignKeyConstraint(
             ['sprint_id'],
             ['project.sprints.id'],
-            name=op.f('fk_tasks_sprint_id_sprints'
-                      ),
+            name=op.f('fk_tasks_sprint_id_sprints')
         ),
         sa.ForeignKeyConstraint(
             ['status_id'],
             ['project.task_status.id'],
-            name=op.f('fk_tasks_status_id_task_status'),
+            name=op.f('fk_tasks_status_id_task_status')
         ),
         sa.PrimaryKeyConstraint(
             'id',
-            name=op.f('pk_tasks'),
+            name=op.f('pk_tasks')
         ),
-        schema='project',
+        schema='project'
     )
 
 
